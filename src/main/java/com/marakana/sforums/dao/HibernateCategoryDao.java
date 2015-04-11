01220a6889993a3f27d1eee4b3198c94b63d63ac
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2015-04-11 07:43:48 -0400

    Updated solution for lab 08 (Spring Security). Step11: Added AOP-based security configuration (annotation @Secured) and added security constraints on HibernateCategoryDao.

--- src/main/java/com/marakana/sforums/dao/HibernateCategoryDao.java
+++ src/main/java/com/marakana/sforums/dao/HibernateCategoryDao.java
@@ -4,6 +4,7 @@ package com.marakana.sforums.dao;
 import java.util.List;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.marakana.sforums.domain.Category;
@@ -20,5 +21,19 @@ public class HibernateCategoryDao extends AbstractHibernateDao<Category> impleme
     @Transactional(readOnly = true)
     public List<Category> getAll() throws DataAccessException {
         return super.findAll("from Category order by name");
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    @Secured("ROLE_ADMIN")
+    public void save(Category entity) throws DataAccessException {
+        super.save(entity);
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    @Secured("ROLE_ADMIN")
+    public void delete(Category entity) throws DataAccessException {
+        super.delete(entity);
     }
 }
